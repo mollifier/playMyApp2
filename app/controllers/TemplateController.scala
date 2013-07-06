2401c1268223c6a61917a566281ebcd6ce6d32a1
mollifier <mollifier@gmail.com>
2013-07-06 14:51:28 +0900

    DB接続

--- app/controllers/TemplateController.scala
+++ app/controllers/TemplateController.scala
@@ -2,6 +2,11 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
+
+import org.squeryl.PrimitiveTypeMode._
+import org.squeryl.Schema
+import org.squeryl.annotations.Column
 
 object TemplateController extends Controller {
   def show = Action {
@@ -11,7 +16,12 @@ object TemplateController extends Controller {
 
   def showDetail(id: Long) = Action {
     var userList = List[String]("mollifier", "user2", "user3")
-    Ok(views.html.list("show list detail " + id, userList))
+    inTransaction {
+      val user = CoreSchema.users.where(u => u.id === id).single
+      Ok(views.html.list("show list detail " + user.name, userList))
+    }
+
+    //Ok(views.html.list("show list detail " + id, userList))
   }
 }
 
